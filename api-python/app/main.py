a226b373217975162d547d4a73ec6aa1350db756
Eric Ranzani <eric_ranzani@yahoo.com.br>
2026-03-26 13:35:55 -0300

    refatorado o codigo para implementar estrutura modular, junto ja acresentando a rota e logica de atualizar dados

---
+++ api-python/app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.database import engine
+from app import models
+from app.routes import ativos
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Monitoramento de Ativos API")
+
+# Configurar CORS para permitir que o Angular acesse a API sem problemas de bloqueio de origem cruzada (CORS)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
+
+# Inclui as rotas modularizadas
+app.include_router(ativos.router)
+
+@app.get("/")
+def home():
+    return {"status": "Online", "version": "1.0.0"}
